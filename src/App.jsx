2b94566445f16bd95a722382ecc4eff73db33723
0-harshit-0 <59227793+0-harshit-0@users.noreply.github.com>
2025-03-31 13:50:14 +0530

    part 1 complete
    only open on streema domain

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import History from './pages/History';
@@ -6,7 +6,31 @@ import Tasks from './pages/Task';
 import Login from './pages/Login'; 
 import ProtectedRoute from './auth/ProtectedRoute';
 
+const domain = 'https://streema.com';
+
+
 const App = () => {
+  useEffect(() => {
+    chrome.runtime.onMessage.addListener(message => {
+      console.log(message, "message")
+      // Might not be as easy if there are multiple side panels open
+      if (message.action === 'closeSidePanel') {
+        window.close();
+      }
+    
+      return true;
+    })
+    
+    chrome.tabs.query({active:true, currentWindow:true}).then(tabs => {
+      const tab = tabs[0];
+      console.log(tab.url)
+    
+      if (!tab.url.includes(domain)) {
+        window.close();
+      }
+    });
+  }, []);
+
   return (
     <Router>
       <Navbar />
---
+++ src/background/background.js
@@ -0,0 +1,33 @@
+const domain = 'https://streema.com';
+
+// background.js
+chrome.action.onClicked.addListener((tab) => {
+  chrome.sidePanel.open({
+    tabId: tab.id
+  });
+});
+
+
+
+// const expectedSidePanelState = {};
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (!tab.url) return;
+  const url = new URL(tab.url);
+  // Enables the side panel
+  if (url.origin === domain) {
+    await chrome.sidePanel.setOptions({
+      tabId,
+      path: 'index.html',
+      enabled: true
+    });
+  } else {
+    // expectedSidePanelState[tabId] = false;
+    await chrome.runtime.sendMessage({ action: "closeSidePanel" });
+    // Disables the side panel on all other sites
+    await chrome.sidePanel.setOptions({
+      tabId,
+      enabled: false
+    });
+  }
+});
+
